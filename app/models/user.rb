a5f6de24563ee13ccb550e6298d085b8a609e6d6
Jorge Calvo G. <jorgeecg10@gmail.com>
2023-11-20 16:48:25 -0500

    Adding UserRoom model with relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :user_rooms
+  has_many :rooms, through: :user_rooms
 end
