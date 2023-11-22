94be6ab7d10b4f03e5ab5ae61e096f7799198127
Jorge Calvo G. <jorgeecg10@gmail.com>
2023-11-22 18:31:31 -0500

    Adding Message models and relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
 
   has_many :user_rooms
   has_many :rooms, through: :user_rooms
+  has_many :messages
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_20_214434) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_22_232047) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "messages", force: :cascade do |t|
+    t.string "content"
+    t.bigint "user_id", null: false
+    t.bigint "room_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["room_id"], name: "index_messages_on_room_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
 
   create_table "rooms", force: :cascade do |t|
     t.string "name"
@@ -41,6 +51,8 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_20_214434) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "messages", "rooms"
+  add_foreign_key "messages", "users"
   add_foreign_key "user_rooms", "rooms"
   add_foreign_key "user_rooms", "users"
 end
